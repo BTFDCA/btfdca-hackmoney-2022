50ecd4565da4decda005cb76e60675d8938df495
omnifi <omnifient@gmail.com>
2022-05-10 14:39:56 +0100

    frontend - improving ui
    - connect with metamask
    - added code to link with superfluid

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,167 @@
+import { Framework } from "@superfluid-finance/sdk-core";
+import { ethers } from "ethers";
+import { useEffect, useState } from "react";
 import "./App.css";
 
+async function createDCAFlow() {
+  // TODO: missing parameters (the flowrate, the target contract)
+  console.log("Creating the DCA flow");
+
+  const provider = new ethers.providers.Web3Provider(window.ethereum);
+  const signer = provider.getSigner();
+
+  const chainId = await window.ethereum.request({ method: "eth_chainId" });
+  const sf = await Framework.create({
+    chainId: Number(chainId),
+    provider: provider,
+  });
+  console.log("got the sf object", sf);
+
+  // TODO: I think this is going to be a super app
+  // const DAIxContract = await sf.loadSuperToken("fDAIx");
+  // const DAIx = DAIxContract.address;
+
+  // TODO: is constant flow agreement the right agreement for DCA? or is there some other agreement?
+  // try {
+  //   const createFlowOperation = sf.cfaV1.createFlow({
+  //     receiver: recipient,
+  //     flowRate: flowRate,
+  //     superToken: DAIx,
+  //     // userData?: string
+  //   });
+
+  //   console.log("Creating your stream...");
+
+  //   const result = await createFlowOperation.exec(signer);
+  //   console.log(result);
+
+  //   console.log(
+  //     `Congrats - you've just created a money stream!
+  //   View Your Stream At: https://app.superfluid.finance/dashboard/${recipient}
+  //   Network: Kovan
+  //   Super Token: DAIx
+  //   Sender: 0xDCB45e4f6762C3D7C61a00e96Fb94ADb7Cf27721
+  //   Receiver: ${recipient},
+  //   FlowRate: ${flowRate}
+  //   `
+  //   );
+  // } catch (error) {
+  //   console.log(
+  //     "Hmmm, your transaction threw an error. Make sure that this stream does not already exist, and that you've entered a valid Ethereum address!"
+  //   );
+  //   console.error(error);
+  // }
+}
+
 function App() {
+  const [currentAccount, setCurrentAccount] = useState("");
+  // TODO: set default values (following the order from the dropdowns)
+  const [buyAmount, setBuyAmount] = useState();
+  const [srcToken, setSourceToken] = useState();
+  const [targetToken, setTargetToken] = useState();
+  const [buyCadence, setBuyCadence] = useState();
+
+  const connectWallet = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        alert("Get MetaMask!");
+        return;
+      }
+      const accounts = await ethereum.request({
+        method: "eth_requestAccounts",
+      });
+      console.log("Connected", accounts[0]);
+      setCurrentAccount(accounts[0]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const checkIfWalletIsConnected = async () => {
+    const { ethereum } = window;
+
+    if (!ethereum) {
+      console.log("Make sure you have metamask!");
+      return;
+    } else {
+      console.log("We have the ethereum object", ethereum);
+    }
+
+    const accounts = await ethereum.request({ method: "eth_accounts" });
+    const chain = await window.ethereum.request({ method: "eth_chainId" });
+    let chainId = chain;
+    console.log("chain ID:", chain);
+    console.log("global Chain Id:", chainId);
+    if (accounts.length !== 0) {
+      const account = accounts[0];
+      console.log("Found an authorized account:", account);
+      setCurrentAccount(account);
+    } else {
+      console.log("No authorized account found");
+    }
+  };
+
+  const setupDCAFlow = async () => {
+    console.log(
+      `setup an agreement to trade ${buyAmount} ${srcToken} for ${targetToken} every ${buyCadence}`
+    );
+    // TODO: convert the input values into calculation amounts
+    // const amountInWei = ethers.BigNumber.from(amount);
+    // const monthlyAmount = ethers.utils.formatEther(amountInWei.toString());
+    // const calculatedFlowRate = monthlyAmount * 3600 * 24 * 30;
+
+    // TODO: perform validations - i.e. check if the wallet has the funds etc
+    // TODO: disable the button and show some info msg
+
+    // TODO: do stuff - create the superfluid stream, etc
+    createDCAFlow();
+
+    // TODO:
+    // redirect the user somewhere if DCA is set up
+    // show an error message if something goes wrong
+  };
+
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  }, []);
+
   return (
     <div className="App">
+      {/* logo stuff */}
+      <div className="logoWrapper">
+        <div>Buy Crypto Assets the Smart Way!</div>
+        <div>DCA is the Way.</div>
+      </div>
+
+      {/* the dca configuration */}
       <div className="dcaWrapper">
         <span>I want to buy</span>
         <span>
-          <input type="number" step="100" placeholder="100" />
+          <input
+            type="number"
+            step="100"
+            placeholder="100"
+            onChange={(e) => setBuyAmount(e.target.value)}
+          />
         </span>
         <span>
-          <select>
+          <select onChange={(e) => setSourceToken(e.target.value)}>
             <option value="dai">DAI</option>
             <option value="usdc">USDC</option>
           </select>
         </span>
         <span>worth of</span>
         <span>
-          <select>
+          <select onChange={(e) => setTargetToken(e.target.value)}>
             <option value="wbtc">wBTC</option>
             <option value="weth">wETH</option>
           </select>
         </span>
         <span>every</span>
         <span>
-          <select>
+          <select onChange={(e) => setBuyCadence(e.target.value)}>
             <option value="month">month</option>
             <option value="week">week</option>
             <option value="day">day</option>
@@ -31,8 +169,27 @@ function App() {
         </span>
         <span>.</span>
       </div>
+
+      {/* error / info messages */}
+      <div className="alertWrapper">
+        <div>
+          if something goes wrong, or we want to alert the user about something
+        </div>
+
+        <div>TODO: calculate how much in funds the wallet must have</div>
+      </div>
+
+      {/* action buttons */}
       <div className="lfgWrapper">
-        <button id="lfgButton">LFG!</button>
+        {currentAccount === "" ? (
+          <button id="connectWallet" className="button" onClick={connectWallet}>
+            Connect Wallet
+          </button>
+        ) : (
+          <button id="lfgButton" onClick={setupDCAFlow}>
+            LFG!
+          </button>
+        )}
       </div>
     </div>
   );
